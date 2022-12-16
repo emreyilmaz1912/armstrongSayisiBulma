45108537917bc885fb722429af3fc958b4ab06fd
emreyilmaz1912 <emreyilmaz264@gmail.com>
2022-12-17 02:02:56 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen bir sayı giriniz :");
+        int number = input.nextInt();;
+        int numberDigit = 0;
+        int tempNumber, numberValue, numberPow,result = 0;
+
+        tempNumber = number;
+        while (tempNumber > 0){
+            tempNumber /= 10;
+            numberDigit++;
+        }
+        tempNumber = number;
+        while (tempNumber > 0){
+            numberValue = tempNumber % 10;
+
+            numberPow = 1;
+
+            for (int i = 1; i <= numberDigit; i++){
+                numberPow *= numberValue;
+            }
+            result +=numberPow;
+            tempNumber /= 10;
+        }
+
+        if (number == result){
+            System.out.println(number + " sayısı bir armstrong sayıdır.");
+        }else {
+            System.out.println(number + " sayısı armstrong sayısı değildir.");
+        }
+
     }
 }
